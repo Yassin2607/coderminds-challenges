803f920687585f856c01f1f179e46f2374e3ffcb
Yassin Rahou <yassin.rahou@hva.nl>
2024-09-23 14:56:14 +0200

    findDuplicateNumber

---
+++ leetcode_287/src/main/java/Main.java
@@ -0,0 +1,12 @@
+package main.java;
+
+import java.lang.reflect.Array;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 3, 4, 2, 2};
+        System.out.println(solution.findDuplicate(nums));
+    }
+}
+
